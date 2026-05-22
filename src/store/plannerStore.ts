e386a742d0e6888d0ca3b6c342bb0f501dc42cdf
Yujin Kim <yujinkim0206@gmail.com>
2026-05-22 22:43:50 +0900

    feat: zustand 스토어

---
+++ src/store/plannerStore.ts
@@ -0,0 +1,52 @@
+import { create } from 'zustand';
+import type { StudyBlock } from '../types';
+import { getWeekStart } from '../utils/time';
+
+interface PlannerState {
+  draftBlocks: StudyBlock[];
+  isDirty: boolean;
+  currentWeekStart: Date;
+}
+
+interface PlannerActions {
+  initDraft: (blocks: StudyBlock[]) => void;
+  addBlock: (block: StudyBlock) => void;
+  updateBlock: (block: StudyBlock) => void;
+  removeBlock: (id: string) => void;
+  setWeek: (date: Date) => void;
+}
+
+export const usePlannerStore = create<PlannerState & PlannerActions>((set) => ({
+  draftBlocks: [],
+  isDirty: false,
+  currentWeekStart: getWeekStart(new Date()),
+
+  initDraft: (blocks) =>
+    set({
+      draftBlocks: blocks,
+      isDirty: false,
+    }),
+
+  addBlock: (block) =>
+    set((state) => ({
+      draftBlocks: [...state.draftBlocks, block],
+      isDirty: true,
+    })),
+
+  updateBlock: (block) =>
+    set((state) => ({
+      draftBlocks: state.draftBlocks.map((b) => (b.id === block.id ? block : b)),
+      isDirty: true,
+    })),
+
+  removeBlock: (id) =>
+    set((state) => ({
+      draftBlocks: state.draftBlocks.filter((b) => b.id !== id),
+      isDirty: true,
+    })),
+
+  setWeek: (date) =>
+    set({
+      currentWeekStart: getWeekStart(date),
+    }),
+}));
